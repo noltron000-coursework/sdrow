3678ae9b4f985e519b0aa2b3a41d3eb951d6745c
Nolan Kovacik <noltron000@gmail.com>
2019-07-10 23:59:29 -0700

    coding challenges completed
    for the FEW libraries class

---
+++ README.md
@@ -0,0 +1,11 @@
+1.  Write a function that uppercases the first character of string
+1. Write a function that uppercases the first letter of each word in a string
+1. Write a function that uppercases every other letter
+1. Write a function that removes all whitespace from a given string
+1. Write a function that removes only the extra whitespace from a given string (example: `“ a        b ” → “a b”`)
+1. Write a function that makes all characters uppercase. (this is the same as .toUppercase())
+1. Write a function that removes extra spaces and replaces spaces with the hyphen "-", and makes all characters lowercase.
+1. Removes extra space and replaces spaces with an underscore "_", and makes all characters lowercase.
+1. Lowercases the first character of the first word. Then uppercases the first character of all other words, and removes all spaces.
+
+[Homework 1](https://github.com/Make-School-Courses/FEW-2.1-Writing-JavaScript-Libraries/blob/master/Assignments/assignment-1-string-lib.md)
---
+++ problems.js
@@ -0,0 +1,89 @@
+// PROBLEM 1
+String.prototype.upperFirst = function () {
+	return this.charAt(0).toUpperCase() + this.slice(1)
+}
+
+String.prototype.lowerFirst = function () {
+	return this.charAt(0).toLowerCase() + this.slice(1)
+}
+
+// PROBLEM 2
+String.prototype.upperWord = function () {
+	const wordList = this.split(" ")
+	let finalString = ""
+	wordList.forEach((word) => {
+		const result = word.upperFirst()
+		finalString = finalString.concat(result, " ")
+	})
+	return finalString.slice(0, -1)
+}
+
+// PROBLEM 3
+String.prototype.upperWiggle = function () {
+	let finalString = ""
+	Array.from(this).forEach((letter, index) => {
+		if (index % 2 === 0) {
+			letter = letter.toUpperCase()
+		}
+		finalString = finalString.concat(letter)
+	})
+	return finalString
+}
+
+// PROBLEM 4
+String.prototype.removeSpaces = function () {
+	return this.replace(/\s/g,'')
+}
+
+// PROBLEM 5
+String.prototype.trimSpaces = function () {
+	return this
+		.split(/\s+/)
+		.join(' ')
+		.trim()
+}
+
+// PROBLEM 6
+String.prototype.allCaps = function () {
+	return this.toUpperCase()
+}
+
+// PROBLEM 7
+String.prototype.kababCase = function () {
+	return this
+		.toLowerCase()
+		.split(/\s+/)
+		.join(' ')
+		.trim()
+		.replace(/\s+/g,'-')
+}
+
+// PROBLEM 8
+String.prototype.snakeCase = function () {
+	return this
+		.toLowerCase()
+		.split(/\s+/)
+		.join(' ')
+		.trim()
+		.replace(/\s+/g,'_')
+}
+
+// PROBLEM 9
+String.prototype.camelCase = function () {
+	return this
+		.trimSpaces()
+		.upperWord()
+		.removeSpaces()
+		.lowerFirst()
+}
+
+
+console.log("PROBLEM 1:","javascript libraries".upperFirst())
+console.log("PROBLEM 2:","javascript libraries".upperWord())
+console.log("PROBLEM 3:","javascript libraries".upperWiggle())
+console.log("PROBLEM 4:","  javascript  libraries        ".removeSpaces())
+console.log("PROBLEM 5:","  javascript  libraries        ".trimSpaces())
+console.log("PROBLEM 6:","javascript libraries".allCaps())
+console.log("PROBLEM 7:","javascript libraries".kababCase())
+console.log("PROBLEM 8:","javascript libraries".snakeCase())
+console.log("PROBLEM 9:","javascript libraries".camelCase())
