3741b42f9ffc084838d087ade8c9b47b45b4e516
Nolan <noltron000@gmail.com>
2019-10-31 12:56:08 -0700

    [fix] exports

--- sdrow.ts
+++ sdrow.ts
@@ -1,17 +1,17 @@
-const capWord = (word: string): string => {
+export const capWord = (word: string): string => {
 	return word.charAt(0).toUpperCase()
 	+ word.slice(1).toLowerCase()
 }
 
-const upperWord = (word: string): string => {
+export const upperWord = (word: string): string => {
 	return word.toUpperCase()
 }
 
-const lowerWord = (word: string): string => {
+export const lowerWord = (word: string): string => {
 	return word.toLowerCase()
 }
 
-const jiggleWord = (word: string): string => {
+export const jiggleWord = (word: string): string => {
 	let result: string = ''
 	Array.from(word).forEach((letter, index) => {
 		if (index % 2 === 0) {
@@ -24,7 +24,7 @@ const jiggleWord = (word: string): string => {
 	return result
 }
 
-const wiggleWord = (word: string): string => {
+export const wiggleWord = (word: string): string => {
 	let result: string = ''
 	Array.from(word).forEach((letter, index) => {
 		if (index % 2 === 1) {
@@ -37,14 +37,14 @@ const wiggleWord = (word: string): string => {
 	return result
 }
 
-const removeSpaces = (sentence: string): string => {
+export const removeSpaces = (sentence: string): string => {
 	// replace any number of spaces with an empty string.
 	// `/\s+` selects consecutive occurences of whitespace.
 	// `/g` stands for a global search.
 	return sentence.replace(/\s+/g, '')
 }
 
-const trimSpaces = (sentence: string): string => {
+export const trimSpaces = (sentence: string): string => {
 	// replace any number of spaces with a single space.
 	// then, trim extra spaces before/after the sentence.
 	// `/\s+` selects consecutive occurences of whitespace.
@@ -55,7 +55,7 @@ const trimSpaces = (sentence: string): string => {
 // ==TODO==
 // determine what `type` that a function is;
 // it is passed in as a parameter!
-const caseCase = (sentence: string, lambda: any): string => {
+export const caseCase = (sentence: string, lambda: any): string => {
 	let result: string = ''
 	const words: string[] = sentence.split(' ')
 	for (let word of words) {
@@ -65,19 +65,19 @@ const caseCase = (sentence: string, lambda: any): string => {
 	return result
 }
 
-const capCase = (sentence: string): string => {
+export const capCase = (sentence: string): string => {
 	return caseCase(sentence, capWord)
 }
 
-const upperCase = (sentence: string): string => {
+export const upperCase = (sentence: string): string => {
 	return caseCase(sentence, upperWord)
 }
 
-const lowerCase = (sentence: string): string => {
+export const lowerCase = (sentence: string): string => {
 	return caseCase(sentence, lowerWord)
 }
 
-const kababCase = (sentence: string): string => {
+export const kababCase = (sentence: string): string => {
 	let result: string = trimSpaces(lowerCase(sentence))
 	// replace all spaces with hyphens.
 	// `/\s` selects one whitespace character unit.
@@ -85,7 +85,7 @@ const kababCase = (sentence: string): string => {
 	return result.replace(/\s/g, '-')
 }
 
-const snakeCase = (sentence: string): string => {
+export const snakeCase = (sentence: string): string => {
 	let result: string = trimSpaces(lowerCase(sentence))
 	// replace all spaces with underscores.
 	// `/\s` selects one whitespace character unit.
@@ -93,32 +93,13 @@ const snakeCase = (sentence: string): string => {
 	return result.replace(/\s/g, '_')
 }
 
-const camelCase = (sentence: string): string => {
+export const camelCase = (sentence: string): string => {
 	let result: string = removeSpaces(capCase(sentence))
 	result = result.charAt(0).toLowerCase()
 	+ result.slice(1)
 	return result
 }
 
-const pascelCase = (sentence: string): string => {
+export const pascelCase = (sentence: string): string => {
 	return removeSpaces(capCase(sentence))
-}
-
-export default {
-	capWord,
-	upperWord,
-	lowerWord,
-	jiggleWord,
-	wiggleWord,
-	removeSpaces,
-	trimSpaces,
-	capCase,
-	upperCase,
-	lowerCase,
-	kababCase,
-	snakeCase,
-	camelCase,
-	pascelCase,
-	// // these are just helper functions
-	// caseCase,
 }
